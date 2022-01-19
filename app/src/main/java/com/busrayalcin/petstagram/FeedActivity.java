f54f140f9bce625d8715d32aa8464a915b075a1a
Büşra KIZILASLAN YALÇIN <busra_kizilaslan@hotmail.com>
2022-01-19 23:07:08 +0300

    Initial commit

---
+++ app/src/main/java/com/busrayalcin/petstagram/FeedActivity.java
@@ -0,0 +1,86 @@
+package com.busrayalcin.petstagram;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.busrayalcin.petstagram.databinding.ActivityFeedBinding;
+import com.busrayalcin.petstagram.databinding.ActivityUploadBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
+
+public class FeedActivity extends AppCompatActivity {
+    private ActivityFeedBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFeedBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        getData();
+
+    }
+    private void getData(){
+        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null){
+                    Toast.makeText(FeedActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+                if (value != null){
+                   for (DocumentSnapshot snapshot : value.getDocuments()){
+                     Map<String, Object> data = snapshot.getData();
+                     String comment = (String) data.get("comment");
+                     String userEmail = (String) data.get("useremail");
+                     String downloadUrl = (String) data.get("downloadurl");
+
+                       System.out.println(comment);
+
+                   }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_post){
+            Intent intent = new Intent(FeedActivity.this,UploadActivity.class);
+            startActivity(intent);
+
+        }else if (item.getItemId() == R.id.signout){
+            auth.signOut();
+            Intent intent = new Intent(FeedActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
